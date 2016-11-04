5c25b3c876b1cda75a4a6b76051f6cc4f171698a
Michael <michael@michaelhazani.com>
2016-11-04 15:06:06 -0700

    test bash change

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,7 +1,9 @@
 const THREE = require("three-js")();
 // const OrbitControls = require("three-orbit-controls")(THREE);
 const datGui = require("dat-gui");
+const request = require('request');
 
+let sphere;
 
 //boilerplate
 var container = document.createElement('div');
@@ -14,6 +16,7 @@ let moonMesh;
 
 let loader = new THREE.JSONLoader();
 
+let pointLight;
 
 function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
@@ -30,15 +33,29 @@ function init() {
     // controls = new OrbitControls(camera, renderer.domElement);
 
 
-    //lights
-    // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    // directionalLight.position.set(-640, 320, 30);
-    // scene.add(directionalLight);
-
-    const pointLight = new THREE.PointLight(0xffffff, 3);
-    pointLight.position.set(0, 0, 300);
+    //light
+    pointLight = new THREE.PointLight(0xffffff, 2, 0);
+    pointLight.position.set(0, 0, 100);
     scene.add(pointLight);
 
+    // let ambiLight = new THREE.AmbientLight(0xffffff, 2);
+    // ambiLight.position.set(0,0,100);
+    // scene.add(ambiLight);
+
+
+    //DAT GUI
+    const gui = new datGui.GUI();
+    gui.add(pointLight.position, 'x', -500, 500);
+    gui.add(pointLight.position, 'y', -500, 500);
+    gui.add(pointLight.position, 'z', -500, 500);
+
+
+
+    //test sphere
+    const sphereGeo = new THREE.BoxGeometry(20,20,20);
+    const sphereMat = new THREE.MeshPhongMaterial({color: 0xff00ff});
+    sphere = new THREE.Mesh(sphereGeo, sphereMat);
+    scene.add(sphere);
 
     //sexy moon
     const material = new THREE.MeshStandardMaterial({
@@ -47,15 +64,11 @@ function init() {
     });
     loader.load('./models/Moon1.json', function(geometry) {
         moonMesh = new THREE.Mesh(geometry, material);
+        // moonMesh.scale.set(10,10,10);
         scene.add(moonMesh);
     });
 
 
-    //DAT GUI
-    const gui = new datGui.GUI();
-    gui.add(pointLight.position, 'x', -10000, 10000);
-    gui.add(pointLight.position, 'y', -10000, 10000);
-    // gui.add(pointLight.position, 'z', -2000, 2000);
 
 
 // end INIT
@@ -72,6 +85,10 @@ function animate() {
         moonMesh.rotation.y -= 0.0001;
     }
     // controls.update();
+//     console.log(pointLight.position.x, pointLight.position.y, pointLight.position.z);
+//     sphere.position.x = pointLight.position.x;
+// sphere.position.y = pointLight.position.y;
+// sphere.position.z = pointLight.position.z;
 }
 
 function onWindowResize() {
@@ -80,6 +97,11 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-
-window.onload = init();
-render();
+//fetch current moon phase data
+request('http://api.burningsoul.in/moon', function(err, res, body) {
+  if(!err &&res.statusCode == 200) {
+    console.log(body);
+  }
+  init();
+ render();
+});
